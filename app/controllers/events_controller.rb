e192392a6f880e72053e495d86f303fbaf6ae8f9
Victoire De Gonneville <v.dechampvallier@gmail.com>
2020-11-24 18:02:05 +0100

    controller done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,25 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [ :show, :edit, :update, :destroy]
   def new
     @event = Event.new
+  end
+
+  def index
+    @events = Event.all
   end
 
   def create
     @event = Event.new(event_params)
     @event.user = current_user
-    if @event.save
-      redirect_to events_path(@event)
+    if @event.save!
+      redirect_to events_path
     else
+
       render :new
     end
+  end
+
+  def show
   end
 
   def edit
@@ -18,20 +27,18 @@ class EventsController < ApplicationController
 
   def update
     @event.update(event_params)
-    redirect_to
-    events_path
+    redirect_to events_path
   end
 
   def destroy
     @event.destroy
-    redirect_to
-    events_path
+    redirect_to events_path
   end
 
   private
 
   def event_params
-    params.require(:event).permit(:name, :description, :starting_time, :ending_time)
+    params.require(:event).permit(:name, :description, :starting_time, :ending_time, :category_id)
   end
 
   def set_event
