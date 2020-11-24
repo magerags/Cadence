c5194c1df0bc42b59aa9fd1dcd574b60e28c4c4b
Tom Whittle <tomwhittle@hey.com>
2020-11-24 15:26:17 +0100

    spelling mistake

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :user_setting, dependant: :destroy
-  has_many :events, dependant: :destroy
+  has_one :user_setting, dependent: :destroy
+  has_many :events, dependent: :destroy
 end
