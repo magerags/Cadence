d25c3c88356f8ba13f588b393cb1d32b3e1b52c7
Victoire De Gonneville <v.dechampvallier@gmail.com>
2020-11-25 16:54:31 +0100

    dashboard&co

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,14 @@
 class Event < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  def self.amount_of_hours(current_user)
+    hours = current_user.events.where("date(starting_time) >= date(?)", Date.today).pluck(:starting_time, :ending_time)
+    hours.reduce(0.0) do |acc, hour|
+     time = (hour[1] - hour[0]) / 3600
+     acc += time
+     acc
+
+    end
+end
 end
--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -1,3 +1,17 @@
 class UserSetting < ApplicationRecord
   belongs_to :user
+
+  def work_hours(current_user)
+
+    worki = current_user.user_setting.work_start
+    worko = current_user.user_setting.work_end
+    ((worko - worki) * 5) / 3600
+  end
+
+  def sleeping_hours(current_user)
+    bedi = current_user.user_setting.bed_time
+    waki = current_user.user_setting.wake_time
+    ((bedi - waki) * 7) / 3600
+
+  end
 end
