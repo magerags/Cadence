f73372d5a43a3a6b395822ad1606fa187bb80700
Victoire De Gonneville <v.dechampvallier@gmail.com>
2020-11-27 14:56:21 +0100

    first shot

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,42 @@
 class GoalsController < ApplicationController
+  before_action :set_goal, only: [:edit, :update, :destroy]
+
+
+  def index
+    @goal = Goal.all
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    @goal.user = current_user
+    if @goal.save
+      redirect_to goals_path
+    else
+
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @goal.update(goal_params)
+    redirect_to goals_path
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to goals_path
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:goaltime, :category_id)
+  end
+
+  def set_goal
+    @goal = Goal.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_132221) do
+ActiveRecord::Schema.define(version: 2020_11_27_134659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_11_26_132221) do
     t.time "goal"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.time "goaltime"
     t.index ["category_id"], name: "index_goals_on_category_id"
     t.index ["user_settings_id"], name: "index_goals_on_user_settings_id"
   end
