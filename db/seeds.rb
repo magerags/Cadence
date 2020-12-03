69d417e265fbb15abe3012c7151b6093734ccdff
Tom Whittle <tomwhittle@hey.com>
2020-12-03 16:32:12 +0100

    to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,47 +10,8 @@ puts "running seeds..."
 
 UserSetting.destroy_all
 Event.destroy_all
+Goal.destroy_all
 Category.destroy_all
 User.destroy_all
-
-tom = User.create!(email: 'tom@tom.com', password: '123456')
-
-tom_settings = UserSetting.create!(
-  user: tom,
-  bed_time: Time.new(1970, 1, 1, 22, 0, 0),
-  wake_time: Time.new(1970, 1, 1, 8, 0, 0),
-  work_start: Time.new(1970, 1, 1, 9, 0, 0),
-  work_end: Time.new(1970, 1, 1, 17, 0, 0)
-  )
-
-Category.create!(name: 'Socialising')
-Category.create!(name: 'Eating')
-Category.create!(name: 'Hobby')
-Category.create!(name: 'Chores')
-Category.create!(name: 'Chilling')
-Category.create!(name: 'Coding')
-Category.create!(name: 'Gym')
-Category.create!(name: 'Running')
-
-
-Event.create!(
-  user: tom,
-  category: Category.all.sample,
-  name: 'Test event 1',
-  description: 'This is a test of the events model',
-  repeatable: false,
-  starting_time: DateTime.now,
-  ending_time: DateTime.now + 1.hours,
-  )
-
-Event.create!(
-  user: tom,
-  category: Category.all.sample,
-  name: 'Test event 2',
-  description: 'This is the second test of the events model',
-  repeatable: false,
-  starting_time: DateTime.now + 2.hours,
-  ending_time: DateTime.now + 4.hours,
-  )
 
 puts "done!"
