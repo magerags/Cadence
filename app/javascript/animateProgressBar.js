81f69edb44c252cd11d9e019a60daca5c0574af2
Tom Whittle <tomwhittle@hey.com>
2020-12-14 15:18:42 +0100

    cleaning up indentation on html files

--- app/javascript/animateProgressBar.js
+++ app/javascript/animateProgressBar.js
@@ -1,18 +1,14 @@
-function animateProgressBar(el, width){
-    el.animate(
-        {width: width},
-        {
-            duration: 2000,
-            step: function(now, fx) {
-                if(fx.prop == 'width') {
-                    el.html(el.data("name") + ": " + Math.round(now * 100) / 100 + '%');
-                }
+function animateProgressBar(el, width) {
+    el.animate({ width: width }, {
+        duration: 2000,
+        step: function(now, fx) {
+            if (fx.prop == "width") {
+                el.html(el.data("name") + ": " + Math.round(now * 100) / 100 + "%");
             }
-        }
-    );
+        },
+    });
 }
 
-
-$('.progress').each(function(){
-   animateProgressBar($(this).find("div"), $(this).data("width"))
+$(".progress").each(function() {
+    animateProgressBar($(this).find("div"), $(this).data("width"));
 });
--- app/javascript/toggleNav.js
+++ app/javascript/toggleNav.js
@@ -1,20 +1,15 @@
-
-
-function toggleNav(){
-  const navElements = document.querySelectorAll('.nav-item');
-const currentPath =  window.location.pathname;
-console.log(navElements);
-if (navElements) {
-  for (let element of navElements){
- let elementPath = element.querySelector('a').pathname;
- if (elementPath == currentPath){
-  element.classList.add("active");
- }
+function toggleNav() {
+    const navElements = document.querySelectorAll(".nav-item");
+    const currentPath = window.location.pathname;
+    console.log(navElements);
+    if (navElements) {
+        for (let element of navElements) {
+            let elementPath = element.querySelector("a").pathname;
+            if (elementPath == currentPath) {
+                element.classList.add("active");
+            }
+        }
+    }
 }
-
-}
-
-}
-
 
 export default toggleNav;
